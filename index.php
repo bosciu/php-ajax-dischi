a65dda6848a34f61674d9ab2bdfd6b34e04a95cf
bosciu <bosciu@icloud.com>
2021-06-30 15:15:10 +0200

    Creazione struttura base con db, header & footer

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    require __DIR__ . "/db/databese.php";
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Spotify albums</title>
+</head>
+<body>
+    <?php require __DIR__ . "/container/header.php" ?>
+        <main>
+            
+        </main>
+    <?php require __DIR__ . "/container/footer.php" ?>
+</body>
+</html>
